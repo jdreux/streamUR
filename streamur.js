c77f004ef63428b52dab4536856b1eccd724d63d
Julien Dreux <julien.dreux@gmail.com>
2012-06-23 10:06:27 -0400

    basic structure

---
+++ streamur.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+var app = express.createServer();
+
+app.configure(function(){
+    app.use(express.methodOverride());
+    app.use(express.bodyParser());
+	app.use(express.cookieParser());
+	app.use(express.session({ secret: "keyboard cat" }));
+	app.use(express.static(__dirname + '/public'));
+    app.use(app.router);
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.listen(80);
+
+console.log("StreamUR listening on port 80.");
