ef9eb02abcc023dfed3355f39f45f7ac103ac257
Stephen R. Hamilton <stephen.r.hamilton@gmail.com>
2012-06-23 13:58:08 -0400

    adding stuff to adapter

--- adapter.js
+++ adapter.js
@@ -1,4 +1,5 @@
 var fs = require("fs");
+var http = require("http");
 
 var streams = exports.streams = {};
 
@@ -13,11 +14,11 @@ JavascriptAdapter.prototype = {
     this._fileName = name;
   },
 
-  openStream: function() {
+  openStream: function(callback) {
     stream = fs.createReadStream(this._fileName); 
-	stream.type = this.type;
-	stream.pause();
-    return stream;
+    stream.type = this.type;
+    stream.pause();
+    callback(stream);
   }
 }
 
@@ -28,4 +29,44 @@ JavascriptAdapter.add = function(name, filename) {
 JavascriptAdapter.add("stream1","stream1.js");
 JavascriptAdapter.add("stream2","stream2.js");
 
+function TwitterAdapter(name, options) {
+  this.init(name,options)
+}
+
+TwitterAdapter.prototype = {
+  type: "twitter",
+
+  init: function(name,options) {
+    this._name = name;
+    this._username = options["username"];
+    this._password = options["password"];
+    this.options = options;
+  },
+
+  openStream: function(callback) {
+    var auth = this._username + ':' + this._password;
+    var twitter_options = {
+      host: "stream.twitter.com",
+      auth: auth,
+      method: "POST",
+      path: "/1/statuses/filter.json",
+      headers: {
+        track: this.options["track"]
+      }
+    };
+    var req = http.request(twitter_options, function(res) {
+      res.type = "twitter";
+      callback(res);
+    }
+  }
+
+}
+
+TwitterAdapter.add = function(name, options) {
+  streams[name] = new TwitterAdapter(name,options);
+}
+
+TwitterAdapter.add("streamUr",{username: "streamUr",password: "streamUr", track:"test"});
+
 exports.JavascriptAdapter = JavascriptAdapter;
+exports.TwitterAdapter = TwitterAdapter;
