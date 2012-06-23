679e84ccca5fdf88cc630a95b57472576f2399c9
Stephen R. Hamilton <stephen.r.hamilton@gmail.com>
2012-06-23 12:09:28 -0400

    there fixed

--- adapter.js
+++ adapter.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 
-export.streams = {"jquery":JavascriptAdapter("jquery.js")};
+var streams = exports.streams = {};
 
 function JavascriptAdapter(name) {
   this.init(name);
@@ -18,3 +18,12 @@ JavascriptAdapter.prototype = {
     return stream;
   }
 }
+
+JavascriptAdapter.add = function(name, filename) {
+  streams[name] = new JavascriptAdapter(filename);
+}
+
+JavascriptAdapter.add("stream1","stream1.js");
+JavascriptAdapter.add("stream2","stream2.js");
+
+exports.JavascriptAdapter = JavascriptAdapter;
