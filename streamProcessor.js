e3f4c7304ee78d351b9ba86b778ad22c9357358c
Thomas Getgood <thomas.getgood@gmail.com>
2012-06-23 11:06:18 -0400

    Basic stream processor structure complete.
    All processors are currently no-ops.

--- streamProcessor.js
+++ streamProcessor.js
@@ -1,26 +1,55 @@
-// Define processor objects
+// 
 
-var processorList = [];
+var processorList = {};
 
 function StreamProcessor(inType, outType, processFunction) {
   this.inputType = inType;
   this.outputTye = outType;
+  this.name = 'unknown';
   
-  this.run = function(args
+  this.validate = function(type) {
+    return type === this.inputType;
+  };
 
+  this.run = function(stream) {
+    if (stream.type !== this.inputType) {
+      throw new Exception("mismatched types in " + this.name);
+    }
+    return processFunction(stream);
+  };
 }
 
 StreamProcessor.prototype = {
 }
 
-function createProcessor(inType, outType, fun, name) {
+function createProcessor(inType, outType, name, fun) {
   var proc = new StreamProcessor(inType, outType, fun);
-  exports[name] = proc;
-  processorList.append(name);
+  proc.name = name;
+  processorList[name] = proc;
 }
 
-exports.listProcessors = function() {
-  return processorList;
-}
+exports.listProcessors = processorList;
 
-exports.StreamProcessor = StreamProcesso
+
+// Define processors.
+//
+var cat = {
+  name : "cat",
+  inputType : 'js',
+  outputType : 'js',
+  validate : function(type) {
+    return type === cat.inputType;
+  },
+  run: function(stream1, stream2) {
+  }
+}
+processorList['cat'] = cat;
+
+createProcessor('js', 'js', 'min', function(stream) {
+});
+
+createProcessor('js', 'js', 'nocache', function(stream) {
+});
+
+createProcessor('js', 'js', 'gzip', function(stream) {
+}):
